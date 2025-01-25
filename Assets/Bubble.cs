cb15a0870ff45d55688143fbd2a3e8d0a298559d
Sebastien Gaumin <sgaumin@gmail.com>
2025-01-25 22:28:12 +0900

    Setup bubble shield

--- Assets/Bubble.cs
+++ Assets/Bubble.cs
@@ -1,34 +1,101 @@
 using UnityEngine;
 using TadaLib.ActionStd;
+using System.Linq;
 
 public class Bubble : MonoBehaviour
 {
-    private float _burstTimer = BurstTime;
-    private bool _hasRidden = false;
-    private MoveInfoCtrl _moveInfoCtrl = null;
+	[System.Serializable]
+	private struct BubbleShieldConfig
+	{
+		public int shieldLevel;
+		public int probability;
+		public Sprite sprite;
+	}
 
-    private const float BurstTime = 5f;
+	private const float BurstTime = 5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        _moveInfoCtrl = GetComponent<MoveInfoCtrl>();
-    }
+	[Header("Configurations")]
+	[SerializeField] private BubbleShieldConfig[] shieldConfigs;
 
-    // Update is called once per frame
-    void Update()
-    {
-        var isRiding = _moveInfoCtrl.IsRiding();
-        if (isRiding)
-        {
-            _hasRidden = true;
-            _burstTimer -= Time.deltaTime;
-        }
+	[Header("References")]
+	[SerializeField] private SpriteRenderer shieldSpriteRenderer;
 
-        if ((!isRiding && _hasRidden)
-            || _burstTimer < 0)
-        {
-            Destroy(gameObject);
-        }
-    }
+	private float _burstTimer = BurstTime;
+	private bool _hasRidden = false;
+	private MoveInfoCtrl _moveInfoCtrl = null;
+	private BubbleShieldConfig currentShieldConfig;
+	private int currentShieldValue;
+
+	void Start()
+	{
+		_moveInfoCtrl = GetComponent<MoveInfoCtrl>();
+
+		int totalProbability = shieldConfigs.Sum(x => x.probability);
+		int pickProbability = Random.Range(0, totalProbability + 1);
+		int progressProbablity = 0;
+		foreach (var shieldConfig in shieldConfigs)
+		{
+			progressProbablity += shieldConfig.probability;
+			if (pickProbability <= progressProbablity)
+			{
+				currentShieldConfig = shieldConfig;
+				break;
+			}
+		}
+
+		currentShieldValue = currentShieldConfig.shieldLevel;
+		UpdateShield();
+	}
+
+	void Update()
+	{
+		var isRiding = _moveInfoCtrl.IsRiding();
+		if (isRiding)
+		{
+			_hasRidden = true;
+			_burstTimer -= Time.deltaTime;
+
+			if (_burstTimer < 0)
+			{
+				Destroy(gameObject);
+			}
+		}
+
+		if (!isRiding && _hasRidden)
+		{
+			currentShieldValue--;
+
+			if (currentShieldValue == 0)
+			{
+				Destroy(gameObject);
+				return;
+			}
+
+			UpdateShield();
+
+			_burstTimer = BurstTime;
+			_hasRidden = false;
+		}
+	}
+
+	private void UpdateShield()
+	{
+		if (currentShieldValue <= 0)
+		{
+			shieldSpriteRenderer.gameObject.SetActive(false);
+			return;
+		}
+
+		BubbleShieldConfig config = default;
+		foreach (var shieldConfig in shieldConfigs)
+		{
+			if (shieldConfig.shieldLevel == currentShieldValue)
+			{
+				config = shieldConfig;
+				break;
+			}
+		}
+
+		shieldSpriteRenderer.sprite = config.sprite;
+	}
 }
