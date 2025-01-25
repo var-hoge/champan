42db588f28ecd4e11c17a569d96cd4131467cbf5
Sebastien Gaumin <sgaumin@gmail.com>
2025-01-25 17:58:51 +0900

    Add actions

--- Assets/PlayerInputHandler.cs
+++ Assets/PlayerInputHandler.cs
@@ -1,19 +1,26 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+	public Action<Vector2> OnMove;
+	public Action OnAction;
+
 	[SerializeField] private PlayerInput playerInput;
 
 	public void Move(InputAction.CallbackContext callback)
 	{
 		Vector2 value = callback.ReadValue<Vector2>();
+		OnMove?.Invoke(value);
 		Debug.Log($"Player {playerInput.playerIndex}: Move {value}");
 	}
 
 	public void DoAction(InputAction.CallbackContext callback)
 	{
 		if (!callback.performed) return;
+
+		OnAction?.Invoke();
 		Debug.Log($"Player {playerInput.playerIndex}: Action");
 	}
 }
