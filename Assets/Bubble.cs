4d04e81c147be55cafaf38b4c4705b8dd54f79f6
tada <tatata0389@gmail.com>
2025-01-27 16:52:14 +0900

    fix the bug that crown bubble is sometimes hidden

--- Assets/Bubble.cs
+++ Assets/Bubble.cs
@@ -8,189 +8,199 @@ using Scripts.Actor.Player;
 
 public class Bubble : MonoBehaviour
 {
-	[Serializable]
-	private struct BubbleShieldConfig
-	{
-		public int shieldLevel;
-		public Sprite sprite;
-	}
+    [Serializable]
+    private struct BubbleShieldConfig
+    {
+        public int shieldLevel;
+        public Sprite sprite;
+    }
 
-	public static Bubble CrownBubble { get; set; }
-	public static int CrownShieldValue { get; private set; }
+    public static Bubble CrownBubble { get; set; }
+    public static int CrownShieldValue { get; private set; }
 
-	private const float BurstTime = 5f;
+    private const float BurstTime = 5f;
 
-	public Action OnDestroyEvent;
+    public Action OnDestroyEvent;
 
-	public bool IsSpawning { get; private set; }
+    public bool IsSpawning { get; private set; }
 
-	[Header("Configurations")]
-	[SerializeField] private int crownStartShield;
-	[SerializeField] private BubbleShieldConfig[] shieldConfigs;
+    [Header("Configurations")]
+    [SerializeField] private int crownStartShield;
+    [SerializeField] private BubbleShieldConfig[] shieldConfigs;
 
-	[SerializeField] private float minSize = 2.2f;
-	[SerializeField] private float maxSize = 4.5f;
+    [SerializeField] private float minSize = 2.2f;
+    [SerializeField] private float maxSize = 4.5f;
 
-	[Header("References")]
-	[SerializeField] private SpriteRenderer shieldSpriteRenderer;
-	[SerializeField] private SpriteRenderer crownSpriteRenderer;
+    [Header("References")]
+    [SerializeField] private SpriteRenderer shieldSpriteRenderer;
+    [SerializeField] private SpriteRenderer crownSpriteRenderer;
 
-	[Header("Amplitude")]
-	[SerializeField] private float amplitude = 0.01f;
+    [Header("Amplitude")]
+    [SerializeField] private float amplitude = 0.01f;
 
-	private float _burstTimer;
-	private bool _hasRidden = false;
-	private MoveInfoCtrl _moveInfoCtrl = null;
-	private BubbleShieldConfig currentShieldConfig;
-	private int currentShieldValue;
-	private Transform visualRoot;
-	private bool _vibrating = false;
-	private IEnumerator _vibrateCoroutine = null;
-	private int currentRiders;
+    private float _burstTimer;
+    private bool _hasRidden = false;
+    private MoveInfoCtrl _moveInfoCtrl = null;
+    private BubbleShieldConfig currentShieldConfig;
+    private int currentShieldValue;
+    private Transform visualRoot;
+    private bool _vibrating = false;
+    private IEnumerator _vibrateCoroutine = null;
+    private int currentRiders;
 
-	public static int LastCrownRidePlayerIdx = 0;
+    public static int LastCrownRidePlayerIdx = 0;
 
-	public Transform CrownSpriteRenderer => crownSpriteRenderer.transform;
-	private bool HasCrown => CrownBubble == this;
+    public Transform CrownSpriteRenderer => crownSpriteRenderer.transform;
+    private bool HasCrown => CrownBubble == this;
 
-	void Start()
-	{
-		_moveInfoCtrl = GetComponent<MoveInfoCtrl>();
-		visualRoot = transform.Find("VisualRoot").transform;
+    void Start()
+    {
+        _moveInfoCtrl = GetComponent<MoveInfoCtrl>();
+        visualRoot = transform.Find("VisualRoot").transform;
 
-		_burstTimer = BurstTime;
+        _burstTimer = BurstTime;
 
-		if (TryGetComponent(out SortingGroup sortingGroup))
-			sortingGroup.sortingOrder = UnityEngine.Random.Range(0, 10000);
+        if (TryGetComponent(out SortingGroup sortingGroup))
+            sortingGroup.sortingOrder = UnityEngine.Random.Range(0, 10000);
 
-		transform.localScale = Vector3.one * UnityEngine.Random.Range(minSize, maxSize);
-	}
+        transform.localScale = Vector3.one * UnityEngine.Random.Range(minSize, maxSize);
+    }
 
-	void OnDestroy()
-	{
-		OnDestroyEvent?.Invoke();
-	}
+    void OnDestroy()
+    {
+        OnDestroyEvent?.Invoke();
+    }
 
-	void Update()
-	{
-		if (GameSequenceManager.Instance.PhaseKind != GameSequenceManager.Phase.Battle)
-		{
-			return;
-		}
+    void Update()
+    {
+        if (GameSequenceManager.Instance.PhaseKind != GameSequenceManager.Phase.Battle)
+        {
+            return;
+        }
 
-		bool losingRider = currentRiders > _moveInfoCtrl.RideObjects.Count;
-		currentRiders = _moveInfoCtrl.RideObjects.Count;
+        bool losingRider = currentRiders > _moveInfoCtrl.RideObjects.Count;
+        currentRiders = _moveInfoCtrl.RideObjects.Count;
 
-		var isRiding = _moveInfoCtrl.IsRiding();
-		if (isRiding)
-		{
-			if (HasCrown)
-			{
-				LastCrownRidePlayerIdx = _moveInfoCtrl.RideObjects[0].GetComponent<DataHolder>().PlayerIdx;
-			}
-			_burstTimer -= Time.deltaTime;
-			_hasRidden = true;
+        var isRiding = _moveInfoCtrl.IsRiding();
+        if (isRiding)
+        {
+            if (HasCrown)
+            {
+                LastCrownRidePlayerIdx = _moveInfoCtrl.RideObjects[0].GetComponent<DataHolder>().PlayerIdx;
+            }
+            _burstTimer -= Time.deltaTime;
+            _hasRidden = true;
 
-			if (_burstTimer < 1.5 && !_vibrating)
-			{
-				_vibrateCoroutine = Vibrate();
-				StartCoroutine(_vibrateCoroutine);
-				_vibrating = true;
-			}
+            if (_burstTimer < 1.5 && !_vibrating)
+            {
+                _vibrateCoroutine = Vibrate();
+                StartCoroutine(_vibrateCoroutine);
+                _vibrating = true;
+            }
 
-			if (_burstTimer < 0)
-			{
-				Destroy(gameObject);
-			}
-		}
+            if (_burstTimer < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
 
-		if ((!isRiding && _hasRidden) || losingRider)
-		{
-			if (HasCrown)
-			{
-				CrownShieldValue--;
+        if ((!isRiding && _hasRidden) || losingRider)
+        {
+            if (HasCrown)
+            {
+                CrownShieldValue--;
 
-				// TODO: Push back the player
-			}
+                // TODO: Push back the player
+            }
 
-			Destroy(gameObject);
-		}
-	}
+            Destroy(gameObject);
+        }
 
-	private IEnumerator Vibrate()
-	{
-		var sign = 1f;
-		while (true)
-		{
-			yield return new WaitForSeconds(0.03f);
-			visualRoot.localPosition = new(amplitude * sign, 0, 0);
-			sign *= -1;
-		}
-	}
+        // prevent bubble from staying at the bottom of the screen
+        {
+            var camera = Camera.main;
+            var bottomRight = camera.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, camera.nearClipPlane));
+            if (transform.position.y < bottomRight.y)
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1f * Time.timeScale);
+            }
+        }
+    }
 
-	private void UpdateShield()
-	{
-		if (currentShieldValue <= 0)
-		{
-			shieldSpriteRenderer.gameObject.SetActive(false);
-			return;
-		}
-	}
+    private IEnumerator Vibrate()
+    {
+        var sign = 1f;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.03f);
+            visualRoot.localPosition = new(amplitude * sign, 0, 0);
+            sign *= -1;
+        }
+    }
 
-	public static void SetupCrown(Bubble bubble)
-	{
-		if (CrownBubble != null)
-		{
-			CrownBubble.RemoveCrown();
-		}
-		else
-		{
-			CrownShieldValue = bubble.crownStartShield;
-		}
+    private void UpdateShield()
+    {
+        if (currentShieldValue <= 0)
+        {
+            shieldSpriteRenderer.gameObject.SetActive(false);
+            return;
+        }
+    }
 
-		CrownBubble = bubble;
+    public static void SetupCrown(Bubble bubble)
+    {
+        if (CrownBubble != null)
+        {
+            CrownBubble.RemoveCrown();
+        }
+        else
+        {
+            CrownShieldValue = bubble.crownStartShield;
+        }
 
-		bubble.SetupCrown(CrownShieldValue);
-	}
+        CrownBubble = bubble;
 
-	private void RemoveCrown()
-	{
-		// TODO: If supporting bubble shield, reset original shield here
+        bubble.SetupCrown(CrownShieldValue);
+    }
 
-		crownSpriteRenderer.gameObject.SetActive(false);
-		shieldSpriteRenderer.gameObject.SetActive(false);
-	}
+    private void RemoveCrown()
+    {
+        // TODO: If supporting bubble shield, reset original shield here
 
-	private void SetupCrown(int shieldValue)
-	{
-		gameObject.name = "BubbleCrown";
+        crownSpriteRenderer.gameObject.SetActive(false);
+        shieldSpriteRenderer.gameObject.SetActive(false);
+    }
 
-		BubbleShieldConfig config = default;
-		foreach (var shieldConfig in shieldConfigs)
-		{
-			if (shieldConfig.shieldLevel > shieldValue) continue;
+    private void SetupCrown(int shieldValue)
+    {
+        gameObject.name = "BubbleCrown";
 
-			config = shieldConfig;
-			break;
-		}
+        BubbleShieldConfig config = default;
+        foreach (var shieldConfig in shieldConfigs)
+        {
+            if (shieldConfig.shieldLevel > shieldValue) continue;
 
-		crownSpriteRenderer.gameObject.SetActive(true);
+            config = shieldConfig;
+            break;
+        }
 
-		shieldSpriteRenderer.gameObject.SetActive(true);
-		shieldSpriteRenderer.sprite = config.sprite;
-	}
+        crownSpriteRenderer.gameObject.SetActive(true);
 
-	public void Init(float x)
-	{
-		IsSpawning = true;
-		GetComponent<Rigidbody2D>().AddForce(new Vector2(x, UnityEngine.Random.Range(15f, 90f)));
-		StartCoroutine(EnableAnimation());
-	}
+        shieldSpriteRenderer.gameObject.SetActive(true);
+        shieldSpriteRenderer.sprite = config.sprite;
+    }
 
-	private IEnumerator EnableAnimation()
-	{
-		yield return new WaitForSeconds(5f);
-		GetComponent<BubbleAnimator>().AnimationEnabled = true;
-		IsSpawning = false;
-	}
+    public void Init(float x)
+    {
+        IsSpawning = true;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(x, UnityEngine.Random.Range(15f, 90f)));
+        StartCoroutine(EnableAnimation());
+    }
+
+    private IEnumerator EnableAnimation()
+    {
+        yield return new WaitForSeconds(5f);
+        GetComponent<BubbleAnimator>().AnimationEnabled = true;
+        IsSpawning = false;
+    }
 }
