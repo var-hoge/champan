642fcccc7185c6e2fb6270461c64142da350724d
COMPUTA\tatat <tatata0389@gmail.com>
2025-05-25 20:07:29 +0900

    feat: レンダリングパイプラインを URP に変更

---
+++ Assets/Scripts/Graphics/Blur/BlurRenderFuture.cs
@@ -0,0 +1,24 @@
+﻿using Graphics.Blur;
+using UnityEngine.Rendering.Universal;
+
+namespace Graphics.Blur
+{
+    public class BlurRendererFeature : ScriptableRendererFeature
+    {
+        BlurRenderPass blurRenderPass;
+
+        public override void Create()
+        {
+            blurRenderPass = new BlurRenderPass();
+            name = "Blur";
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (blurRenderPass.Setup(renderer))
+            {
+                renderer.EnqueuePass(blurRenderPass);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Graphics/Blur/BlurRenderPass.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine;
+
+namespace Graphics.Blur
+{
+    public class BlurRenderPass : ScriptableRenderPass
+    {
+        private Material material;
+        private BlurSettings blurSettings;
+
+        private RTHandle source;
+        private RTHandle blurTex;
+
+        public bool Setup(ScriptableRenderer renderer)
+        {
+            //source = renderer.cameraColorTargetHandle;
+            //blurSettings = VolumeManager.instance.stack.GetComponent<BlurSettings>();
+            //renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+
+            //if (blurSettings != null && blurSettings.IsActive())
+            //{
+            //    material = new Material(Shader.Find("PostProcessing/Blur"));
+            //    return true;
+            //}
+
+            return false;
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            if (blurSettings == null || !blurSettings.IsActive())
+                return;
+
+            blurTex = RTHandles.Alloc(
+                cameraTextureDescriptor.width,
+                cameraTextureDescriptor.height,
+                colorFormat: GraphicsFormat.R8G8B8A8_UNorm,
+                name: "_BlurTex"
+            );
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            //if (blurSettings == null || !blurSettings.IsActive())
+            //    return;
+
+            //CommandBuffer cmd = CommandBufferPool.Get("Blur Post Process");
+
+            //int gridSize = Mathf.CeilToInt(blurSettings.strength.value * 6.0f);
+            //if (gridSize % 2 == 0) gridSize++;
+
+            //material.SetInteger("_GridSize", gridSize);
+            //material.SetFloat("_Spread", blurSettings.strength.value);
+
+            //cmd.Blit(source, blurTex, material, 0);
+            //cmd.Blit(blurTex, source, material, 1);
+
+            //context.ExecuteCommandBuffer(cmd);
+            //cmd.Clear();
+            //CommandBufferPool.Release(cmd);
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            if (blurTex != null)
+            {
+                RTHandles.Release(blurTex);
+                blurTex = null;
+            }
+        }
+    }
+}
