5307d31193ac564577bfb6aa3f5226391fdd4480
ほげ <fictives73@gmail.com>
2025-06-04 01:02:27 +0900

    バブルが画面に収まるようにする

--- Assets/Scripts/App/Actor/Gimmick/RespawnBubble/PlayerSpawner.cs
+++ Assets/Scripts/App/Actor/Gimmick/RespawnBubble/PlayerSpawner.cs
@@ -17,14 +17,29 @@ namespace App.Actor.Gimmick.RespawnBubble
 		[SerializeField] private int bubbleCountMin = 1;
 		[SerializeField] private int bubbleCountMax = 5;
 
+		private (float Min, float Max) spawnRangeX;
+		private (float Min, float Max) spawnRangeY;
+
 		private List<string> _SEPath = null;
 		private List<string> SEPath => _SEPath ??= GameController.GetSEPath("SE/Player Death/Player_Death_", 9).ToList();
+
+		private static readonly Vector3 OutOfScreenPoint = new(0, -30, 0);
+
 
 		// Start is called once before the first execution of Update after the MonoBehaviour is created
 		void Start()
 		{
 			_moveInfoCtrl = GetComponent<MoveInfoCtrl>();
-			//BGMManager.Instance.Play(BGMPath.FIGHT, _BGMVolumeRate);
+
+			// スポーン範囲のキャッシュ
+            {
+				var camera = Camera.main;
+				var topRight = camera.ScreenToWorldPoint(new(Screen.width, Screen.height, camera.nearClipPlane));
+				var maxX = topRight.x - 1f;
+				var maxY = topRight.y + 0.5f;
+				spawnRangeX = (-maxX, maxX);
+				spawnRangeY = (-maxY, maxY);
+			}
 		}
 
 		// Update is called once per frame
@@ -32,31 +47,32 @@ namespace App.Actor.Gimmick.RespawnBubble
 		{
 			foreach (var player in _moveInfoCtrl.RideObjects)
 			{
-				player.GetComponent<App.Actor.Player.DataHolder>().IsDead = true;
-
+				player.GetComponent<Player.DataHolder>().IsDead = true;
+				// SEの再生
 				var path = SEPath[Random.Range(0, SEPath.Count)];
 				SEManager.Instance.Play(path, 20f);
-
-				// respawn player
-				var camera = Camera.main;
-				var topRight = camera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, camera.nearClipPlane));
-				var x = Mathf.Clamp(player.transform.position.x, -topRight.x, topRight.x);
-				var y = topRight.y + 0.5f;
-				var respawnBubble = Instantiate(_respawnBubble, new(x, y, 0), Quaternion.identity);
+				// リスポーンバブルの生成
+				var spawnPointX = Mathf.Clamp(player.transform.position.x, spawnRangeX.Min, spawnRangeX.Max);
+				var respawnBubble = Instantiate(_respawnBubble, new(spawnPointX, spawnRangeY.Max, 0), Quaternion.identity);
 				respawnBubble.Init(player);
-				player.transform.position = new Vector3(0, -30, 0);
+				// プレイヤーを画面外に移動
+				player.transform.position = OutOfScreenPoint;
+				// 追加のバブルを生成
+				CreateBubble(spawnPointX);
+			}
+		}
 
-				var xPosition = x - 2f;
-				var xForce = -15f;
-
-				int count = Random.Range(bubbleCountMin, bubbleCountMax);
-				for (var n = 0; n < count; ++n)
-				{
-					var bubble = Instantiate(_bubble, new(xPosition, -y, 0), Quaternion.identity);
-					bubble.Init(xForce);
-					xPosition += 2f;
-					xForce += 15f;
-				}
+		private void CreateBubble(float spawnPointX)
+        {
+			var xPosition = spawnPointX - 2f;
+			var xForce = -15f;
+			int count = Random.Range(bubbleCountMin, bubbleCountMax);
+			for (var n = 0; n < count; ++n)
+			{
+				var bubble = Instantiate(_bubble, new(xPosition, spawnRangeY.Min, 0), Quaternion.identity);
+				bubble.Init(xForce);
+				xPosition += 2f;
+				xForce += 15f;
 			}
 		}
 	}
