32055016a9de92fb62da46caa8fb7b612210c0a2
COMPUTA\tatat <tatata0389@gmail.com>
2025-07-28 17:14:27 +0900

    feat: オープニングムービーを再生するように

--- Assets/Scripts/App/Ui/Title/TitleUiManager.cs
+++ Assets/Scripts/App/Ui/Title/TitleUiManager.cs
@@ -9,6 +9,7 @@ using UniRx;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using KanKikuchi.AudioManager;
+using UnityEngine.Video;
 
 namespace App.Ui.Title
 {
@@ -29,6 +30,15 @@ namespace App.Ui.Title
         #endregion
 
         #region privateフィールド
+        [SerializeField]
+        VideoPlayer _introPlayer;
+        [SerializeField]
+        Canvas _introCanvas;
+        [SerializeField]
+        Canvas _mainCanvas;
+
+        bool _isVideoFinished = false;
+
         [SerializeField] private Transform _start;
         [SerializeField] private GameObject _startOnImage;
         [SerializeField] private GameObject _startOffImage;
@@ -37,10 +47,63 @@ namespace App.Ui.Title
         [SerializeField] private GameObject _exitOffImage;
         #endregion
 
-            #region privateメソッド
+        #region privateメソッド
         public async UniTask Staging()
         {
-            // 最低 2 秒は待つ
+            // UI が動画を上書きしてしまったので、再生終了まで非表示にする
+            _mainCanvas.gameObject.SetActive(false);
+
+            // 動画準備開始
+            _introPlayer.Prepare();
+
+            // 動画再生のために、各種システムが安定化するまで待つ
+            await UniTask.WaitForSeconds(0.5f);
+
+            // completed しても動画の読み込みが続いていることがある
+            //_introPlayer.prepareCompleted += (vp) =>
+            //{
+            //    vp.Play();
+            //    // 再生開始までの背景
+            //    _introCanvas.gameObject.SetActive(false);
+            //};
+
+            _introPlayer.loopPointReached += (vp) =>
+            {
+                _isVideoFinished = true;
+            };
+
+            await UniTask.WaitUntil(() => _introPlayer.isPrepared);
+
+            _introPlayer.frame = 0; // 明示的に最初から再生する
+            _introPlayer.Play();
+
+            // 動画が終わるまで待つ
+            var lastClickedTime = -10.0f;
+            while (true)
+            {
+                if (_isVideoFinished)
+                {
+                    break;
+                }
+
+                // A ボタンが連続で二回押されたら終了
+                var inputProxy = TadaLib.Input.PlayerInputManager.Instance.InputProxy(0);
+                if (inputProxy.IsPressedTrigger(TadaLib.Input.ButtonCode.Action))
+                {
+                    if (Time.time - lastClickedTime < 0.5f)
+                    {
+                        _introPlayer.Stop();
+                        break;
+                    }
+
+                    lastClickedTime = Time.time;
+                }
+
+                await UniTask.Yield();
+            }
+
+            _introCanvas.gameObject.SetActive(false);
+            _mainCanvas.gameObject.SetActive(true);
 
             // ロゴのアニメーション
             var main = GameObject.Find("Main").transform;
@@ -104,64 +167,66 @@ namespace App.Ui.Title
 
             await UniTask.WaitForSeconds(0.2f);
 
-            var inputProxy = TadaLib.Input.PlayerInputManager.Instance.InputProxy(0);
-            var isPushed = false;
-            var isStartSelected = true;
-            var isInputValid = true;
-            inputProxy.OnAction += OnActoinTrigged;
-            inputProxy.OnMove += OnMove;
-
-            while (true)
             {
-                if (isPushed)
+                var inputProxy = TadaLib.Input.PlayerInputManager.Instance.InputProxy(0);
+                var isPushed = false;
+                var isStartSelected = true;
+                var isInputValid = true;
+                inputProxy.OnAction += OnActoinTrigged;
+                inputProxy.OnMove += OnMove;
+
+                while (true)
                 {
-                    if (isStartSelected)
+                    if (isPushed)
                     {
-                        // SE再生
-                        SEManager.Instance.Play(SEPath.MENU_VALIDATION);
-                        inputProxy.OnAction -= OnActoinTrigged;
-                        inputProxy.OnMove -= OnMove;
-                        TadaLib.Scene.TransitionManager.Instance.StartTransition("CharaSelect", 0.3f, 0.3f);
-                        break;
-                    }
-                    else
-                    {
+                        if (isStartSelected)
+                        {
+                            // SE再生
+                            SEManager.Instance.Play(SEPath.MENU_VALIDATION);
+                            inputProxy.OnAction -= OnActoinTrigged;
+                            inputProxy.OnMove -= OnMove;
+                            TadaLib.Scene.TransitionManager.Instance.StartTransition("CharaSelect", 0.3f, 0.3f);
+                            break;
+                        }
+                        else
+                        {
 #if UNITY_EDITOR
-                        UnityEditor.EditorApplication.isPlaying = false;
+                            UnityEditor.EditorApplication.isPlaying = false;
 #else
                         Application.Quit();
 #endif
+                        }
                     }
+                    await UniTask.Yield();
                 }
-                await UniTask.Yield();
-            }
 
-            void OnActoinTrigged()
-            {
-                isPushed = true;
-            }
-
-            void OnMove(Vector2 value)
-            {
-                if (!isInputValid || value.x == 0)
+                void OnActoinTrigged()
                 {
-                    isInputValid = true;
-                    return;
+                    isPushed = true;
                 }
 
-                isInputValid = false;
-                isStartSelected = !isStartSelected;
-                // 表示イメージの切替
-                _startOnImage.SetActive(isStartSelected);
-                _startOffImage.SetActive(!isStartSelected);
-                _exitOnImage.SetActive(!isStartSelected);
-                _exitOffImage.SetActive(isStartSelected);
-                // 文字サイズの設定
-                var (startValue, exitvalue) = isStartSelected ? (1.1f, 1f) : (1f, 1.1f);
-                _start.DOScale(Vector3.one * startValue, 0.1f);
-                _exit.DOScale(Vector3.one * exitvalue, 0.1f);
-                // SE再生
-                SEManager.Instance.Play(SEPath.MENU_NAVIGATION);
+                void OnMove(Vector2 value)
+                {
+                    if (!isInputValid || value.x == 0)
+                    {
+                        isInputValid = true;
+                        return;
+                    }
+
+                    isInputValid = false;
+                    isStartSelected = !isStartSelected;
+                    // 表示イメージの切替
+                    _startOnImage.SetActive(isStartSelected);
+                    _startOffImage.SetActive(!isStartSelected);
+                    _exitOnImage.SetActive(!isStartSelected);
+                    _exitOffImage.SetActive(isStartSelected);
+                    // 文字サイズの設定
+                    var (startValue, exitvalue) = isStartSelected ? (1.1f, 1f) : (1f, 1.1f);
+                    _start.DOScale(Vector3.one * startValue, 0.1f);
+                    _exit.DOScale(Vector3.one * exitvalue, 0.1f);
+                    // SE再生
+                    SEManager.Instance.Play(SEPath.MENU_NAVIGATION);
+                }
             }
         }
 
